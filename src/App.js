36caf0f5934a4f0de7f5d54a2262c0bc2c825349
Dhanya <dhanu.nova@gmail.com>
2021-05-07 19:11:47 +1000

    Create react app and redux integration in app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router ,Route,Switch} from 'react-router-dom'
+import Header from './components/Header';
+import  ProductList  from './components/ProductList';
+import  ProductDetail  from './components/ProductDetail';
+import './index.css'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="App">
+            This is App
+            <Router >
+                <Header />
+                <Switch>
+                <Route path='/' exact component={ ProductList} />
+                <Route path='/product/:productId' exact component={ ProductDetail} />
+                <Route>404 Not Found</Route>
+                </Switch>
+            </Router>
+        </div>
+    )
 }
 
-export default App;
+export default App
